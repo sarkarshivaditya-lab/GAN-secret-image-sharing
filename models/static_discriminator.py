a8f9a095569cc98bd8bb41ba6e948e8f6e514e3c
Shivaditya Sarkar <sarkar.shivaditya@gmail.com>
2026-09-15 11:29:49 +0530

    Strengthen spatial static discriminator

--- models/static_discriminator.py
+++ models/static_discriminator.py
@@ -3,31 +3,28 @@ import torch.nn as nn
 
 
 class StaticDiscriminator(nn.Module):
-    """Distinguishes learned shares from uniform random TV-static noise."""
+    """Distinguishes learned shares from independent uniform random noise."""
 
     def __init__(self):
         super().__init__()
-
-        self.network = nn.Sequential(
+        self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
-
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
+            nn.InstanceNorm2d(128, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
-
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(256),
+            nn.InstanceNorm2d(256, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
-
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
+            nn.InstanceNorm2d(512, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
-
-            nn.AdaptiveAvgPool2d((1, 1)),
         )
-        self.classifier = nn.Linear(512, 1)
+        self.classifier = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(256, 1, kernel_size=3, padding=1),
+        )
 
     def forward(self, share):
-        features = self.network(share).flatten(start_dim=1)
-        return self.classifier(features)
+        return self.classifier(self.features(share)).mean(dim=(2, 3))
