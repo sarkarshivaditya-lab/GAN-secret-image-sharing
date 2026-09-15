269b9221d26dbedf8a3af301e8395fbd12c91df2
Shivaditya Sarkar <sarkar.shivaditya@gmail.com>
2026-09-15 11:29:35 +0530

    Redesign encoder for noise-like balanced shares

--- models/encoder.py
+++ models/encoder.py
@@ -1,65 +1,45 @@
 import torch
 import torch.nn as nn
 
+
 class ShareEncoder(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.InstanceNorm2d(64, affine=True),
             nn.ReLU(inplace=True),
-
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
+            nn.InstanceNorm2d(128, affine=True),
             nn.ReLU(inplace=True),
-
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True)
+            nn.InstanceNorm2d(256, affine=True),
+            nn.ReLU(inplace=True),
         )
 
         self.share_generator = nn.Sequential(
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True)
+            nn.InstanceNorm2d(256, affine=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.InstanceNorm2d(256, affine=True),
+            nn.ReLU(inplace=True),
         )
 
-        self.share1 = nn.Sequential(
-            nn.Conv2d(256, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 3, kernel_size=3, padding=1),
-            nn.Sigmoid()
-        )
-
-        self.share2 = nn.Sequential(
-            nn.Conv2d(256, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 3, kernel_size=3, padding=1),
-            nn.Sigmoid()
-        )
-
-        self.share3 = nn.Sequential(
-            nn.Conv2d(256, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 3, kernel_size=3, padding=1),
-            nn.Sigmoid()
-        )
-
-        self.share4 = nn.Sequential(
-            nn.Conv2d(256, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 3, kernel_size=3, padding=1),
-            nn.Sigmoid()
-        )
+        self.share_heads = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv2d(256, 64, kernel_size=3, padding=1),
+                nn.InstanceNorm2d(64, affine=True),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 32, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(32, 3, kernel_size=3, padding=1),
+                nn.Sigmoid(),
+            )
+            for _ in range(4)
+        ])
 
     def forward(self, x):
-        features = self.features(x)
-        features = self.share_generator(features)
-
-        share1 = self.share1(features)
-        share2 = self.share2(features)
-        share3 = self.share3(features)
-        share4 = self.share4(features)
-
-        return share1, share2, share3, share4
+        features = self.share_generator(self.features(x))
+        return tuple(head(features) for head in self.share_heads)
