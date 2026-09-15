fca135a03c054de51e38669107952d07ff61a819
Shivaditya Sarkar <sarkar.shivaditya@gmail.com>
2026-09-15 11:29:42 +0530

    Strengthen decoder for four-share fusion

--- models/decoder.py
+++ models/decoder.py
@@ -1,50 +1,31 @@
 import torch
 import torch.nn as nn
 
+
 class ShareDecoder(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.decoder = nn.Sequential(
             nn.Conv2d(12, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
+            nn.InstanceNorm2d(256, affine=True),
             nn.ReLU(inplace=True),
-
-            nn.ConvTranspose2d(
-                256, 128,
-                kernel_size=4,
-                stride=2,
-                padding=1
-            ),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.InstanceNorm2d(256, affine=True),
             nn.ReLU(inplace=True),
-
-            nn.ConvTranspose2d(
-                128, 64,
-                kernel_size=4,
-                stride=2,
-                padding=1
-            ),
-            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(128, affine=True),
             nn.ReLU(inplace=True),
-
-            nn.ConvTranspose2d(
-                64, 32,
-                kernel_size=4,
-                stride=2,
-                padding=1
-            ),
-            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(64, affine=True),
             nn.ReLU(inplace=True),
-
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(32, affine=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(32, 3, kernel_size=3, padding=1),
-            nn.Sigmoid()
+            nn.Sigmoid(),
         )
 
     def forward(self, share1, share2, share3, share4):
-        combined = torch.cat(
-            [share1, share2, share3, share4],
-            dim=1
-        )
-
+        combined = torch.cat([share1, share2, share3, share4], dim=1)
         return self.decoder(combined)
